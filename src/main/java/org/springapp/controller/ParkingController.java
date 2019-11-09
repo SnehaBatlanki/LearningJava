c85897646ccd6a103e9e433887f4a7e5cc232a08
Girija Sneha Batlanki <gibatlan@microsoft.com>
2019-11-09 15:46:28 +0530

    Basic app

---
+++ src/main/java/org/springapp/controller/ParkingController.java
@@ -0,0 +1,12 @@
+package org.springapp.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/")
+public class ParkingController {
+
+    @RequestMapping()
+    public
+}
